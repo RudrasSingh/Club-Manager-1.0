a9da3008ec5251c0cfeb4cb261d95b59a1df85c0
Atul <code.atul01@gmail.com>
2024-04-01 00:51:26 +0530

    .

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def landingPage():
     return render_template('signup.html')
+
+@app.route('/homepage')
+def signupPage():
+    return render_template()
+
+
 
 if __name__ == "__main__":
 
-    app.run(debug=True, port = 5000)
+    app.run(debug=True)
     
