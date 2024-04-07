9b923ff73632412b0a3c20bfee0bcb099f3ada8a
Encryption-prince <subhammaity6827@gmail.com>
2024-04-07 13:10:32 +0530

    Preloader Fixed

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -1,11 +1,19 @@
 
+// var preloader = document.querySelector('.loading');
+// preloader.style.display = 'flex'; // Display the preloader
+
+// setTimeout(function() {
+//   preloader.style.display = 'none'; // Hide the preloader
+// }, 5000); //5sec preloader time
+
+
 var preloader = document.querySelector('.loading');
+var body = document.querySelector('body');
+
 preloader.style.display = 'flex'; // Display the preloader
+body.classList.add('loading'); // Add the loading class to body to prevent scrolling
 
 setTimeout(function() {
   preloader.style.display = 'none'; // Hide the preloader
+  body.classList.remove('loading'); // Remove the loading class to enable scrolling
 }, 5000); //5sec preloader time
-
-
-
-
